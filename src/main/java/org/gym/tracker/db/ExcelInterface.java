b45e563fd5af3e456182f4464cd43b49ffceaf92
Adams-97 <elliotadams97@yahoo.co.uk>
2025-05-02 01:07:36 +0100

    Created unit tests and interface for Gryff to start

---
+++ src/main/java/org/gym/tracker/db/ExcelInterface.java
@@ -0,0 +1,43 @@
+package org.gym.tracker.db;
+
+import org.apache.poi.ss.util.AreaReference;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+interface ExcelInterface {
+
+    /**
+     * Use the input stream and other parameters to get an AreaReference from target table
+     */
+    AreaReference getTableAreaReference(InputStream excelFile, String sheetName, String tableName);
+
+    /**
+     * Get all index positions of rows
+     */
+    List<Integer> getColIndexes(AreaReference tableArea);
+
+    /**
+     * Get all index positions of columns
+     */
+    List<Integer> getRowIndexes(AreaReference tableArea);
+
+    /**
+     * Use both indexes to iterate through the table and parse out GymRecords
+     */
+    List<GymRecord> parseTable(List<Integer> rowIndexes, List<Integer> colIndexes);
+
+    /**
+     * Given a gymRecord it writes to the datasource and returns 1 if it wrote successfully
+     */
+    int writeLineOfGymData(GymRecord row);
+
+    /**
+     * Performs writes across list, returning number of successful writes
+     */
+    default List<Integer> writeLinesOfGymData(List<GymRecord> rows) {
+        return rows.stream().map(this::writeLineOfGymData).collect(Collectors.toList());
+    }
+
+}
--- src/main/java/org/gym/tracker/db/ExcelStore.java
+++ src/main/java/org/gym/tracker/db/ExcelStore.java
@@ -1,20 +1,39 @@
 package org.gym.tracker.db;
 
+import org.apache.poi.ss.util.AreaReference;
+
+import java.io.InputStream;
 import java.util.List;
 
-public class ExcelStore implements DataStore {
-
-    //  TODO: Complete the class, adding any extra methods or variables needed
-    //  TODO: Add docstrings and unit tests for all methods added
-    //  TODO: Get unit test in 'DataStoreTest.java' to pass
+public class ExcelStore implements ExcelInterface {
 
     @Override
-    public GymRecord parseLineOfGymData(String lineFromStore) {
+    public AreaReference getTableAreaReference(InputStream excelFile, String sheetName, String tableName) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> getColIndexes(AreaReference tableArea) {
+        return null;
+    }
+
+    @Override
+    public List<Integer> getRowIndexes(AreaReference tableArea) {
+        return null;
+    }
+
+    @Override
+    public List<GymRecord> parseTable(List<Integer> rowIndexes, List<Integer> colIndexes) {
         return null;
     }
 
     @Override
     public int writeLineOfGymData(GymRecord row) {
         return 0;
+    }
+
+    @Override
+    public List<Integer> writeLinesOfGymData(List<GymRecord> rows) {
+        return ExcelInterface.super.writeLinesOfGymData(rows);
     }
 }
