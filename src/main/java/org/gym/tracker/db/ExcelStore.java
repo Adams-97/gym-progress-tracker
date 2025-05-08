f2d5306ac433d9d0db31d4656e82a0e03c1bfbf0
gryff <gryffparish0@gmail.com>
2025-05-08 19:47:17 +0100

    Added code for getting columns and rows into their respective functions

--- src/main/java/org/gym/tracker/db/ExcelStore.java
+++ src/main/java/org/gym/tracker/db/ExcelStore.java
@@ -1,11 +1,24 @@
 package org.gym.tracker.db;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.OLE2NotOfficeXmlFileException;
+import org.apache.poi.poifs.filesystem.NotOLE2FileException;
+import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.util.AreaReference;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelStore implements ExcelInterface {
+
+
+
+
 
     @Override
     public AreaReference getTableAreaReference(InputStream excelFile, String sheetName, String tableName) {
@@ -14,13 +27,41 @@ public class ExcelStore implements ExcelInterface {
 
     @Override
     public List<Integer> getColIndexes(AreaReference tableArea) {
-        return null;
+
+        //
+        int lastColNum = tableArea.getLastCell().getCol();
+
+        //
+        int firstColNum = tableArea.getFirstCell().getCol();
+
+        List<Integer> colIndexList = new ArrayList<>();
+
+           //populates the list with all the numbers leading up to the final column number creating a list of indexes
+           for (int i = firstColNum; i < lastColNum; i++){
+               colIndexList.add(i);
+           }
+
+        return colIndexList;
     }
 
     @Override
     public List<Integer> getRowIndexes(AreaReference tableArea) {
-        return null;
+//
+        int lastRolNum = tableArea.getLastCell().getRow();
+        //
+        int firstRowNum = tableArea.getFirstCell().getRow();
+
+        List<Integer> rowIndexList = new ArrayList<>();
+
+        //populates the list with all the numbers leading up to the final column number creating a list of indexes
+        for (int i = firstRowNum; i < lastRolNum; i++){
+            rowIndexList.add(i);
+        }
+
+        return rowIndexList;
     }
+
+
 
     @Override
     public List<GymRecord> parseTable(List<Integer> rowIndexes, List<Integer> colIndexes) {
